488bdf0b3ecd84aba54761274e939370d15c586b
cycool29 <cycool29@gmail.com>
2022-12-14 09:32:23 +0800

    Update build-whatsapp.sh

--- build-whatsapp.sh
+++ build-whatsapp.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-VERSION="$(($(wget -qO- https://github.com/cycool29/whatsapp-for-linux/releases/latest  | grep -m 1 -o "WhatsApp for Linux.*"  | sed 's/WhatsApp for Linux //g; s/ ·.*//g; s/.0//g') + 1))"
+VERSION="$(($(wget -qO- https://github.com/cycool29/whatsapp-for-linux/releases/latest  | grep -m 1 -o "WhatsApp for Linux.*"  | sed 's/WhatsApp for Linux //g' | head -c 2) + 1))"
 
 echo "if ('serviceWorker' in navigator) {
     caches.keys().then(function (cacheNames) {
